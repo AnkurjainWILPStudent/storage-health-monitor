15ec6bbcc1accf227bf0e79689d63ff0b711e4f8
AnkurjainWILPStudent <2021wa86527@wilp.bits-pilani.ac.in>
2025-11-03 03:36:42 +0530

    test file for disk_monitor.py

--- client_node/test_client.py
+++ client_node/test_client.py
@@ -4,3 +4,7 @@ if __name__ == "__main__":
     run()
     print("Test run complete. Check log for details.")
 
+#test of this client_node/test_client.py
+#!/usr/bin/env python3
+#test against disk_monitor.py
+
